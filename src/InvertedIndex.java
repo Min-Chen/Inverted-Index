982ba6223c740aa9938a4caa728b799be40f943c
Min Chen <chenmin.artemis@gmail.com>
2015-03-01 23:50:53 -0600

    add multi files handler

--- src/InvertedIndex.java
+++ src/InvertedIndex.java
@@ -1,31 +1,59 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 
 /**
  * Created by minchen on 15/2/28.
  */
 public class InvertedIndex {
+    private static HashMap<String, HashMap<String, ArrayList<Integer>>> map = new HashMap<String, HashMap<String, ArrayList<Integer>>>();
+    private static String currentFileName;
+    private static int currentWordIndex;
     public static void main(String[] args)  {
+        getFiles("./");
+    }
+
+    public static String filteredLine(String str){
+        String pattern = "[^a-zA-Z0-9]+";
+        return str.replaceAll(pattern," ");
+    }
+
+
+    public static void getFiles(String path) {
+        File file = new File(path);
+        File[] files = file.listFiles();
+        for (int i = 0; i < files.length; i++) {
+            if(!files[i].isDirectory()){
+                if(files[i].toString().toLowerCase().endsWith(".txt")) {
+                    System.out.println(files[i]);
+                    handleOneFile(files[i].getPath());
+                }
+            }
+            else getFiles(files[i].getPath());
+        }
+    }
+
+    public static void handleOneFile(String path) {
         try {
-            FileReader fr = new FileReader ("src/test.txt");
+            FileReader fr = new FileReader (path);
             BufferedReader br = new BufferedReader(fr);
+            currentFileName = path;
+            currentWordIndex = 1;
 
             String str;
             while ((str= br.readLine()) != null){
-                System.out.println(filteredLine(str));
+                handleWords(filteredLine(str));
             }
             br.close();
         } catch (FileNotFoundException e) {
             System.out.println ("File not found");
         } catch (IOException e) {
             e.printStackTrace();
-            }
+        }
     }
-    public static String filteredLine(String str){
-        String pattern = "[^a-zA-Z0-9]+";
-        return str.replaceAll(pattern," ");
+
+    public static void handleWords(String str) {
     }
+
 }
